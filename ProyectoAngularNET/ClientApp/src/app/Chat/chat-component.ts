00dbb7c4ea9ecd03d5d722c00019849d2afc7eba
Cristobal Mauricio <cristobal.cntcto@gmail.com>
2022-06-27 01:46:12 -0400

    Proyecto Chat Angular con .NET Core

---
+++ ProyectoAngularNET/ClientApp/src/app/Chat/chat-component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'chat-app',
+    templateUrl: './chat-component.html'
+})
+export class ChatComponent {
+    //Atributo
+    public chatMessages: string[] = ["Hola mundo","Cristobal","Mauricio","nuevo elemento"];
+
+}
---
+++ ProyectoAngularNET/ClientApp/src/app/message/message.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-message',
+    templateUrl: './message.component.html'
+})
+export class MessageComponent{
+    //Datos de entrada con input
+    @Input() texto: string;
+}
